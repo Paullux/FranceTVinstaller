238df63a453e7522e86fb78db3b530d4c711379a
Paul WOISARD <paulwoisard@gmail.com>
2022-04-04 19:24:08 +0200

    Update Installer-FranceTV-AppImage.sh

--- Installer-FranceTV-AppImage.sh
+++ Installer-FranceTV-AppImage.sh
@@ -3,7 +3,7 @@ mkdir -p "$HOME/.icons/"
 mkdir -p "$HOME/AppImage/"
 mkdir -p "$HOME/.local/share/applications/"
 
-wget "https://github.com/Paullux/FranceTV/raw/main/images/france-tv-2.png" -O "$HOME/.icons/france-tv-2.png"
+wget "https://github.com/Paullux/FranceTV/raw/main/images/france-tv-2.png" -O "$HOME/.icons/france-tv-2.png"  
 wget "https://github.com/Paullux/FranceTV/releases/download/1.1/FranceTV-1.1-x86_64.AppImage"
 
 chmod +x FranceTV-1.1-x86_64.AppImage
